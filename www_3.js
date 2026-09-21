149bff4b0f77217888742904e426ba9f3c548e95
Andrus Rinde <andrusrinde@users.noreply.github.com>
2026-09-21 13:29:03 +0300

    Loodud erinevad marsruudid ja async faili lugemine

--- www_3.js
+++ www_3.js
@@ -1,15 +1,65 @@
 const http = require('http');
+//moodul päringu parsimiseks
+const url = require('url');
+//moodul failitee haldamiseks
+const path = require('path');
+//moodul failide haldamiseks, ASYNC puhul on vaja seda toetavat erilisemat moodulit
+//const fs = require('fs');
+const fs = require('fs').promises;
 const dateET = require('./src/dateTimeET');
 const pageHead = '<!DOCTYPE html>\n<html lang="et">\n<head>\n\t<meta charset="utf-8">\n\t<title>Andrus Rinde, veebiprogrammeerimine</title>\n</head>\n<body>\n';
 const pageBody = '\t<h1>Andrus Rinde, veebiprogrammeerimine</h1>\n\t <p>See leht on loodud veebiprogrammeerimise kursusel <a href="https://www.tlu.ee">Tallinna Ülikoolis</a> ning ei sislda tõsiseltvõetavat sisu!</p>\n\t<p>Esialgu tutvusime lihtsalt HTML keelega, see leht aga on juba programmeeritud.</p>\n\t<hr>';
+const pageBanner = '<img src="veebiprogrammeerimine_2026_TA.png" alt="">';
 const pageFoot = '\n</body>\n</html>';
 
-http.createServer(function(req, res){
-	res.writeHead(200, {"Content-type": "text/html"});
-	res.write(pageHead);
-	res.write(pageBody);
-	res.write('Täna on ' + dateET.day() + ', ' + dateET.fullDate(1) + ', kell oli lehe avamise hetkel: ' + dateET.fullTime() +'.');
-	res.write(pageFoot);
-	//res.write('Veeb läkski käima!');
-	return res.end();
+http.createServer(async function(req, res){
+	//parsin url-i
+	console.log('Päring: ' + req.url);
+	let currentURL = url.parse(req.url, true);
+	console.log('Parsituna: ' + currentURL.pathname);
+	
+	//hakkame erinevaid lehti jaotama -> routes (marsruudid)
+	
+	if(currentURL.pathname === '/'){	
+		res.writeHead(200, {"Content-type": "text/html"});
+		res.write(pageHead);
+		res.write(pageBanner);
+		res.write(pageBody);
+		res.write('\n\t<p>Täna on ' + dateET.day() + ', ' + dateET.fullDate(1) + ', kell oli lehe avamise hetkel: ' + dateET.fullTime() +'.</p>');
+		res.write('\n\t<ul>');
+		res.write('\n\t\t<li><a href="/vanasona">Tänane vanasõna</a></li>');
+		res.write('\n\t</ul>');
+		res.write(pageFoot);
+		//res.write('Veeb läkski käima!');
+		return res.end();
+	}
+	
+	else if(currentURL.pathname === '/vanasona'){
+		res.writeHead(200, {"Content-type": "text/html"});
+		res.write(pageHead);
+		res.write(pageBanner);
+		res.write('\t<h1>Eesti vanasõnad</h1>\n\t<p>Siin näed tänase päeva vanasõna.</p>\n\t<hr>');
+		res.write('\n\t<p><a href="/">Tagasi avalehele</a></p>');
+		res.write(pageFoot);
+		return res.end();
+	}
+	
+	else if(currentURL.pathname === '/veebiprogrammeerimine_2026_TA.png'){
+		//teeme pildi tegeliku asukoha programmile kättesaadavaks
+		let picPath = path.join(__dirname, 'pic', currentURL.pathname);
+		//let picPath = path.join(__dirname, 'pic');
+		//console.log(picPath);
+		try {
+			const data = await fs.readFile(picPath);
+			res.writeHead(200, {"Content-type": "image/png"});
+			res.end(data);
+		} catch (err){
+			res.writeHead(404, {"Content-type": "text/plain; charset=utf8"});
+			return res.end('Pilti ei leitud!');
+		}
+	}
+	
+	else {
+		res.end('Viga 404, ei leia sellist lehte!');
+	}
 }).listen(5100);
